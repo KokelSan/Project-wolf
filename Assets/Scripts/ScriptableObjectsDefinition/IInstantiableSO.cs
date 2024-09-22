4ca5c6d5abe72779e5f1537f2e839d3e4b969388
Theo Kokel <kokelsan@outlook.com>
2024-09-22 20:56:37 +0200

    changed parentSO type to IInstantiableSO

--- Assets/Scripts/ScriptableObjectsDefinition/IInstantiableSO.cs
+++ Assets/Scripts/ScriptableObjectsDefinition/IInstantiableSO.cs
@@ -1,13 +1,12 @@
-using UnityEngine;
 
 public interface IInstantiableSO
 {
-    ScriptableObject ParentSO { get; }
+    IInstantiableSO ParentSO { get; }
 
     /// <summary>
     /// Useful for debug as it appears when serializing the object in Json for example
     /// </summary>
     int InstanceId { get; }
 
-    void Initialize(ScriptableObject parentSO);
+    void Initialize(IInstantiableSO parentSO);
 }
--- Assets/Scripts/ScriptableObjectsDefinition/InstantiableSO.cs
+++ Assets/Scripts/ScriptableObjectsDefinition/InstantiableSO.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public abstract class InstantiableSO : ScriptableObject, IInstantiableSO
 {
-    public ScriptableObject ParentSO { get; private set; } = null;
+    public IInstantiableSO ParentSO { get; private set; } = null;
 
     public int InstanceId => GetInstanceID();
 
-    public void Initialize(ScriptableObject parentSO)
+    public void Initialize(IInstantiableSO parentSO)
     {
         ParentSO = parentSO;
         Initialize();
